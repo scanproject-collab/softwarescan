f80aa8f5cab4f565ddd2d9d92971dad7873e03ef
joseok <pauloowneto@gmail.com>
2025-02-24 13:32:31 -0300

    Atualizando perfomace

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,6 @@
-import express from 'express';
-import firebaseConfig from './service/firebaseConfig';
-
-const app = express();
+import { app } from "./app";
 const port = 3000;
 
-// Middleware para testar se a conexão está funcionando
-app.get('/', (req, res) => {
-  res.send('API está funcionando!');
-});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
