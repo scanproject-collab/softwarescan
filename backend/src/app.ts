980a283e886745a2ad39c24c20e3944f2a45f392
joseok <pauloowneto@gmail.com>
2025-02-26 18:51:45 -0300

    Atualização de rotas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import authRoutes from './routes/authRoutes';
 
 export const app = express();
+app.use(express.json()); 
+
+app.use('/auth', authRoutes); 
 
 app.get('/', (req, res) => {
-  res.send('API está funcionando!'); 
+  res.send('API está funcionando!');
 });
-
 
 export default app;
