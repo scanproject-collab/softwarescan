e8bf3a8f99490c78deb3429927ded65433ed1364
joseok <pauloowneto@gmail.com>
2025-03-05 21:54:47 -0300

    Criado o arquivo para o usuario realizar o registro na plataforma

---
+++ app/src/app/pages/auth/signup.tsx
@@ -0,0 +1,7 @@
+// pages/auth/index.tsx
+import React from 'react';
+import RegisterScreen from '../../components/auth/signupScreen';
+
+export default function RegisterPage() {
+  return <RegisterScreen />;
+}
