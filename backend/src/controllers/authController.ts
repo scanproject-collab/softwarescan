976c5a5e619f39900d0b3d9456ec7a3367dd85f6
joseok <pauloowneto@gmail.com>
2025-03-07 20:41:49 -0300

    Adição de reutilização d efuncionalidade de notificação

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,37 +1,21 @@
 import { Request, Response } from 'express';
 import { registerUser, loginUser } from '../service/authService';
-import axios from "axios";
+import { sendNotification } from '../utils/oneSignal';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 export const registerController = async (req: Request, res: Response) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, role, playerId } = req.body;
     const user = await registerUser(name, email, password, role);
 
-    if (user.role === 'OPERATOR' && user.isPending) {
-      try {
-        const response = await axios.post(
-          'https://onesignal.com/api/v1/notifications',
-          {
-            app_id: process.env.ONESIGNAL_APP_ID,
-            included_segments: ['All'], // Temporário para teste
-            contents: { en: `Novo pedido de cadastro: ${name} (${email})` },
-            data: { userId: user.id, email },
-          },
-          {
-            headers: {
-              Authorization: `Basic ${process.env.ONESIGNAL_API_KEY}`,
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-        console.log('Notificação enviada com sucesso:', response.data);
-      } catch (error) {
-        console.error('Erro ao enviar notificação ao OneSignal:', error.response?.data || error.message);
-        
-      }
+    if (user.role === 'OPERATOR' && user.isPending && playerId) {
+      await sendNotification(
+        [playerId],
+        `Seu pedido de cadastro foi enviado: ${name} (${email}). Aguarde aprovação.`,
+        { userId: user.id, email }
+      );
     }
 
     res.status(201).json({ message: 'User registered successfully', user });
