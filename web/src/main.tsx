44f12f0c8d2f09262153f348c649022d4d4643db
joseok <pauloowneto@gmail.com>
2025-03-10 19:20:36 -0300

    Atualizado a logica main

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Login from './pages/auth/loginScreen.tsx';
 
+import TagManagement from "./pages/admin/TagManagement.tsx";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />} />
+        <Route path="/admin/tags" element={<TagManagement />}  />
       </Routes>
     </Router>
   </StrictMode>
