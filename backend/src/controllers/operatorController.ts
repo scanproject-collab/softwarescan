3e2bc556bafe5f79a3c439270ae9cfebfab1c21d
joseok <pauloowneto@gmail.com>
2025-03-11 09:38:02 -0300

    Fix bugs

--- backend/src/controllers/operatorController.ts
+++ backend/src/controllers/operatorController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
