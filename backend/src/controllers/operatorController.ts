285062ada1dc760d86176f340d7525636b4ab9c1
joseok <pauloowneto@gmail.com>
2025-03-11 09:26:03 -0300

    Atualizado a funcionalidade de atualizar os dados do operador

--- backend/src/controllers/operatorController.ts
+++ backend/src/controllers/operatorController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -9,22 +11,39 @@ interface RequestWithUser extends Request {
 
 export const updateOperatorAccount = async (req: RequestWithUser, res: Response) => {
   const { email, name, password, institutionId } = req.body;
-  
+
   if (!req.user?.id) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
   try {
+    const data: any = { email, name, institutionId };
+
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      data.password = hashedPassword;
+    }
+
     const updatedUser = await prisma.user.update({
       where: { id: req.user.id },
-      data: {
-        email,
-        name,
-        password, 
-        institutionId,
-      },
+      data,
     });
-    res.status(200).json({ message: 'Account updated successfully', user: updatedUser });
+
+    const token = jwt.sign(
+        {
+          id: updatedUser.id,
+          email: updatedUser.email,
+          name: updatedUser.name,
+          role: updatedUser.role,
+          institutionId: updatedUser.institutionId,
+          createdAt: updatedUser.createdAt,
+        },
+        process.env.JWT_SECRET || 'your_jwt_secret_key',
+        { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ message: 'Account updated successfully', user: updatedUser, token });
   } catch (error: any) {
     res.status(400).json({ message: `Error updating account: ${error?.message || 'Unknown error'}` });
   }
