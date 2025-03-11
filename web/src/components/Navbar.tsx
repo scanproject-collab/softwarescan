0a302e921d2af60291c728b1ed0f07d82302f067
joseok <pauloowneto@gmail.com>
2025-03-11 14:37:37 -0300

    Atualização da rota de tags na Navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar: React.FC = () => {
               Perfil
             </DropdownMenuItem>
             <DropdownMenuItem
-                onClick={() => navigate("/tags")}
+                onClick={() => navigate("/admin/tags")}
                 className="cursor-pointer hover:bg-gray-100"
             >
               Gerenciar Tags
