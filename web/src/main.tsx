d0319582124e3d09059f5a20204d8c0f0ea0da49
Seixass <victorseixasmcz@gmail.com>
2025-03-17 01:35:12 -0300

    Rotas de Perfil e Atualizar dados Adicionadas

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Login from './pages/auth/loginScreen.tsx';
+import UpdateAdmin from './pages/admin/UpdateAdmin.tsx';
+import ProfileAdmin from './pages/admin/ProfileAdmin.tsx';
 
 import TagManagement from "./pages/admin/TagManagement.tsx";
 import 'leaflet/dist/leaflet.css';
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />} />
         <Route path="/admin/tags" element={<TagManagement />}  />
+        <Route path="/admin/profile" element={<ProfileAdmin />} />
+        <Route path="/admin/update" element={<UpdateAdmin />} />
       </Routes>
     </Router>
   </StrictMode>
