b28db7da519377f63e83d31d0c1b8c01011b9149
Seixass <victorseixasmcz@gmail.com>
2025-03-17 23:14:43 -0300

    Rotas para Esqueceu a Senha Adicionada

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,18 +6,26 @@ import App from './App.tsx';
 import Login from './pages/auth/loginScreen.tsx';
 import UpdateAdmin from './pages/admin/UpdateAdmin.tsx';
 import ProfileAdmin from './pages/admin/ProfileAdmin.tsx';
-
 import TagManagement from "./pages/admin/TagManagement.tsx";
+import PasswordRecoveryRequestScreen from './pages/auth/PasswordRecoveryRequestScreen.tsx'; 
+import PasswordRecoverySuccessScreen from './pages/auth/passwordRecoverySuccessScreen.tsx';
+import PasswordResetCodeVerificationScreen from './pages/auth/passwordResetCodeVerificationScreen.tsx';
+import PasswordResetScreen from './pages/auth/passwordResetScreen.tsx';
 import 'leaflet/dist/leaflet.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />} />
-        <Route path="/admin/tags" element={<TagManagement />}  />
+        <Route path="/admin/tags" element={<TagManagement />} />
         <Route path="/admin/profile" element={<ProfileAdmin />} />
         <Route path="/admin/update" element={<UpdateAdmin />} />
+        <Route path="/recovery" element={<PasswordRecoveryRequestScreen />} />
+        <Route path="/recovery/success" element={<PasswordRecoverySuccessScreen />} />
+        <Route path="/recovery/verify-code" element={<PasswordResetCodeVerificationScreen />} />
+        <Route path="/recovery/reset" element={<PasswordResetScreen />} />
       </Routes>
     </Router>
   </StrictMode>
