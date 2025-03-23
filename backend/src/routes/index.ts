8f7d50a81665bb78d31b015d974f8f8d2d1f8082
joseok <pauloowneto@gmail.com>
2025-03-23 12:28:14 -0300

    Novo arquivo criado para englobar arquivos de rotas

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,17 @@
+import authRoutes from './authRoutes';
+import operatorRoutes from './operatorRoutes';
+import adminRoutes from './adminRoutes';
+import managerRoutes from './managerRoutes';
+import institutionRoutes from './institutionRoutes';
+import postRoutes from './postRoutes';
+import tagRoutes from './tagRoutes';
+
+export {
+    authRoutes,
+    operatorRoutes,
+    adminRoutes,
+    managerRoutes,
+    institutionRoutes,
+    postRoutes,
+    tagRoutes
+}
