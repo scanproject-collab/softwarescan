924f95b3040843b4dcde468ad98dbc92e2d5dacc
joseok <pauloowneto@gmail.com>
2025-03-23 12:25:37 -0300

    Criação da rota de polygno

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -11,6 +11,9 @@ import PasswordRecoveryRequestScreen from './pages/auth/PasswordRecoveryRequestS
 import PasswordRecoverySuccessScreen from './pages/auth/passwordRecoverySuccessScreen.tsx';
 import PasswordResetCodeVerificationScreen from './pages/auth/passwordResetCodeVerificationScreen.tsx';
 import PasswordResetScreen from './pages/auth/passwordResetScreen.tsx';
+import UserProfile from './components/UserProfile.tsx';
+import PolygonManagement from './pages/admin/PolygonManagement.tsx';
+import 'leaflet/dist/leaflet';
 import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -22,10 +25,12 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/admin/tags" element={<TagManagement />} />
         <Route path="/admin/profile" element={<ProfileAdmin />} />
         <Route path="/admin/update" element={<UpdateAdmin />} />
+        <Route path="/admin/polygons" element={<PolygonManagement  />} /> {/* Nova rota */}
         <Route path="/recovery" element={<PasswordRecoveryRequestScreen />} />
         <Route path="/recovery/success" element={<PasswordRecoverySuccessScreen />} />
         <Route path="/recovery/verify-code" element={<PasswordResetCodeVerificationScreen />} />
         <Route path="/recovery/reset" element={<PasswordResetScreen />} />
+        <Route path="/user/:userId" element={<UserProfile />} />
       </Routes>
     </Router>
   </StrictMode>
