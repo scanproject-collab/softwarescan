d0ca79534c4a691588dadf430c183c31581c4662
joseok <pauloowneto@gmail.com>
2025-03-24 09:00:38 -0300

    Atualização de segurança, services/api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,3 +1,4 @@
+// services/api.ts
 import axios from "axios";
 
 const api = axios.create({
@@ -5,8 +6,33 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
-    "Authorization": `Bearer ${localStorage.getItem("userToken")}`,
   },
 });
+
+
+api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("userToken");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        console.log("Erro 401 detectado, redirecionando para /login...");
+        localStorage.removeItem("userToken");
+        localStorage.removeItem("userData");
+        window.location.href = "/login";
+      }
+      return Promise.reject(error);
+    }
+);
 
 export default api;
