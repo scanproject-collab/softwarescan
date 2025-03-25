dce8a1343824a57997cb51cdd2359ae96f8618de
joseok <pauloowneto@gmail.com>
2025-03-25 03:33:07 -0300

    Atualização da logica de polygnos

--- backend/src/routes/polygnosRoutes.ts
+++ backend/src/routes/polygnosRoutes.ts
@@ -1,10 +1,12 @@
-import {authMiddleware, roleMiddleware} from "../middlewares/authMiddleware";
-import {createPolygon, deletePolygon, listPolygons, updatePolygon} from "../controllers/polygnosController";
-import {Router} from "express";
+// polygnosRoutes.ts
+import { authMiddleware, roleMiddleware } from "../middlewares/authMiddleware";
+import { createPolygon, deletePolygon, listPolygons, updatePolygon, uploadShapefile, createPolygonFromShapefile } from "../controllers/polygnosController";
+import { Router } from "express";
 
 const router = Router();
 
 router.post('/create', authMiddleware, roleMiddleware(['ADMIN', 'MANAGER']), createPolygon);
+router.post('/create-from-shapefile', authMiddleware, roleMiddleware(['ADMIN', 'MANAGER']), uploadShapefile, createPolygonFromShapefile);
 router.get('/', authMiddleware, roleMiddleware(['ADMIN', 'MANAGER']), listPolygons);
 router.put('/:polygonId', authMiddleware, roleMiddleware(['ADMIN', 'MANAGER']), updatePolygon);
 router.delete('/:polygonId', authMiddleware, roleMiddleware(['ADMIN', 'MANAGER']), deletePolygon);
