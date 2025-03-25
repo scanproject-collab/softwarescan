be56b192dc1b1c3cc69bcf8b6def7a622a14611c
joseok <pauloowneto@gmail.com>
2025-03-25 03:51:03 -0300

    Atualização rotas

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,37 +1,40 @@
+// main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
-import Login from './pages/auth/LoginScreen.tsx';
-import UpdateAdmin from './pages/admin/UpdateAdmin.tsx';
-import ProfileAdmin from './pages/admin/ProfileAdmin.tsx';
-import TagManagement from "./pages/admin/TagManagement.tsx";
-import PasswordRecoveryRequestScreen from './pages/auth/PasswordRecoveryRequestScreen.tsx'; 
-import PasswordRecoverySuccessScreen from './pages/auth/passwordRecoverySuccessScreen.tsx';
-import PasswordResetCodeVerificationScreen from './pages/auth/passwordResetCodeVerificationScreen.tsx';
-import PasswordResetScreen from './pages/auth/passwordResetScreen.tsx';
-import UserProfile from './components/UserProfile.tsx';
-import PolygonManagement from './pages/admin/PolygonManagement.tsx';
+import App from './App';
+import Login from './pages/auth/LoginScreen';
+import UpdateAdmin from './pages/admin/UpdateAdmin';
+import ProfileAdmin from './pages/admin/ProfileAdmin';
+import TagManagement from "./pages/admin/TagManagement";
+import PasswordRecoveryRequestScreen from './pages/auth/PasswordRecoveryRequestScreen';
+import PasswordRecoverySuccessScreen from './pages/auth/passwordRecoverySuccessScreen';
+import PasswordResetCodeVerificationScreen from './pages/auth/passwordResetCodeVerificationScreen';
+import PasswordResetScreen from './pages/auth/passwordResetScreen';
+import UserProfileAdmin from './components/UserProfileAdmin';
+import PolygonManagement from './pages/PolygonManagement.tsx';
+import ProfileManager from './pages/manager/ProfileManager';
 import 'leaflet/dist/leaflet';
 import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<App />} />
-        <Route path="/admin/tags" element={<TagManagement />} />
-        <Route path="/admin/profile" element={<ProfileAdmin />} />
-        <Route path="/admin/update" element={<UpdateAdmin />} />
-        <Route path="/admin/polygons" element={<PolygonManagement  />} /> {/* Nova rota */}
-        <Route path="/recovery" element={<PasswordRecoveryRequestScreen />} />
-        <Route path="/recovery/success" element={<PasswordRecoverySuccessScreen />} />
-        <Route path="/recovery/verify-code" element={<PasswordResetCodeVerificationScreen />} />
-        <Route path="/recovery/reset" element={<PasswordResetScreen />} />
-        <Route path="/user/:userId" element={<UserProfile />} />
-      </Routes>
-    </Router>
-  </StrictMode>
+    <StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<App />} />
+          <Route path="/tags" element={<TagManagement />} />
+          <Route path="/admin/profile" element={<ProfileAdmin />} />
+          <Route path="/manager/profile" element={<ProfileManager />} />
+          <Route path="/admin/update" element={<UpdateAdmin />} />
+          <Route path="/polygons" element={<PolygonManagement />} />
+          <Route path="/recovery" element={<PasswordRecoveryRequestScreen />} />
+          <Route path="/recovery/success" element={<PasswordRecoverySuccessScreen />} />
+          <Route path="/recovery/verify-code" element={<PasswordResetCodeVerificationScreen />} />
+          <Route path="/recovery/reset" element={<PasswordResetScreen />} />
+          <Route path="/user/:userId" element={<UserProfileAdmin />} />
+        </Routes>
+      </Router>
+    </StrictMode>
 );
