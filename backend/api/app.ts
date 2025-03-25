6e504769a8f1e8a834356d389944da222d0823f4
joseok <pauloowneto@gmail.com>
2025-03-25 18:22:18 -0300

    Rebuild vercel

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -25,7 +25,7 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.get("/google-maps-api-url", (_req: Request, res: Response) => {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY || "AIzaSyCfLlShC9EMKLBOlmjCJcxivCeUrvfUinE";
+  const apiKey ="AIzaSyCfLlShC9EMKLBOlmjCJcxivCeUrvfUinE";
   if (!apiKey) {
     return res.status(500).json({ message: "Google Maps API key not configured" });
   }
