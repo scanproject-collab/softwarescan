a7e8b9abb13e725bc11e82565ab26dd131fe037d
joseok <pauloowneto@gmail.com>
2025-03-31 04:42:43 -0300

    Atualização rota teste notificação

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express"; 
 import { authRoutes, operatorRoutes, postRoutes, tagRoutes, managerRoutes, institutionRoutes, adminRoutes, polygnosRoutes } from "../src/routes/index"; 
+import { sendOneSignalNotification } from '../src/services/oneSignalNotification';
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -33,6 +34,22 @@ app.get("/google-maps-api-url", (_req: Request, res: Response) => {
   res.json({ url });
 });
 
+
+app.post("/send-notification"), async (req: Request, res: Response) => {
+  const { playerId, title, body, data } = req.body;
+  if (!playerId || !title || !body) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+  console.log('Sending notification to playerId:', playerId);
+  try {
+    await sendOneSignalNotification(playerId, title, body, data);
+    res.status(200).json({ message: 'Notification sent successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to send notification' });
+  }
+};
+
+
 app.get("/ping", (_req: Request, res: Response) => {
   res.sendStatus(200);
 });
@@ -40,6 +57,8 @@ app.get("/ping", (_req: Request, res: Response) => {
 app.use((_req: Request, res: Response) => {
   res.status(500).send("Not found");
 });
+
+
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error("Error occurred:", err);
