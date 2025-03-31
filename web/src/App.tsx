7c5d8f5e6a73d7d9f011d220324f9a61fba22dc9
Seixass <victorseixasmcz@gmail.com>
2025-03-31 01:19:35 -0300

    Fixed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -217,6 +217,7 @@ const App: React.FC = () => {
                     className="rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-orange-400"
                   >
                     <option value="">Todas as Instituições</option>
+                    
                     {uniqueInstitutions.map((institution: any) => (
                       <option key={institution.id} value={institution.id}>
                         {institution.title}
